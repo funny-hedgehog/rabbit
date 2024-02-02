3d74039f7bd33c995e30f15c9569e01a84bd793b
arifm <arifmho@yandex.ru>
2024-02-02 13:12:18 +0300

    commit 5

--- src/App.js
+++ src/App.js
@@ -1,44 +1,42 @@
 import React from 'react';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { Redirect, Route } from 'react-router';
-import { HelpPage } from './pages/HelpPage';
+import { IonApp, IonHeader, IonToolbar, IonTitle, IonButton } from '@ionic/react';
 import * as Portals from '@ionic/portals';
 import './App.css';
-import {DevPage} from "./pages/DevPage";
 
-function App({ context }) {
+function App() { // { context }
   return (
     <IonApp>
-      <IonReactRouter>
-        <IonRouterOutlet>
-          {context?.startingRoute ?
-            <Route exact path="/">
-              {context.startingRoute === '/' ? <DevPage /> : <Redirect to={context.startingRoute} />}
-            </Route> : null}
-          <Route path="/help" exact component={HelpPage} />
-        </IonRouterOutlet>
-      </IonReactRouter>
+      <h1>HHHHHH</h1>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Inbox</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonButton
+        className="checkout-button"
+        expand="block"
+        onClick={() => {
+          Portals.publish({ topic: 'showCheckout' });
+        }}
+      >
+        Checkout
+      </IonButton>
     </IonApp>
+
+
   );
 }
 
 export default App;
 
 /*
-  <h1>HHHHHH</h1>
-  <IonHeader>
-    <IonToolbar>
-      <IonTitle>Inbox</IonTitle>
-    </IonToolbar>
-  </IonHeader>
-  <IonButton
-    className="checkout-button"
-    expand="block"
-    onClick={() => {
-      Portals.publish({ topic: 'showCheckout' });
-    }}
-  >
-    Checkout
-  </IonButton>
+<IonReactRouter>
+  <IonRouterOutlet>
+    {context?.startingRoute ?
+      <Route exact path="/">
+        {context.startingRoute === '/' ? <DevPage /> : <Redirect to={context.startingRoute} />}
+      </Route> : null}
+    <Route path="/help" exact component={HelpPage} />
+  </IonRouterOutlet>
+</IonReactRouter>
  */
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { getInitialContext } from '@ionic/portals';
+// import { getInitialContext } from '@ionic/portals';
 
-const initialContext = getInitialContext()
-  ?.value ?? { startingRoute: '/' };
+// const initialContext = getInitialContext()
+//   ?.value ?? { startingRoute: '/' };
+//
+// <App context={initialContext} />
 
 ReactDOM.render(
   <React.StrictMode>
-    <App context={initialContext} />
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
